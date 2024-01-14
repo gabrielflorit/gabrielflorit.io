cd209fac60215f3e238cee4cb434056eb42697f9
Gabriel Florit <gabrielflorit@gmail.com>
2024-01-14 13:06:46 -0500

    Add multi-page support

---
+++ src/webgpu/App.tsx
@@ -0,0 +1,9 @@
+import '@/App.css'
+
+function App() {
+  return (
+    <main className="bg-darkest text-lightest font-mono text-xl">webgpu</main>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,12 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        webgpu: resolve(__dirname, 'src/webgpu/index.html'),
+      },
+    },
+  },
 })
